f671585246bafcaaa820e05966aa4d258e89d347
catbutter-HWS <u1124029@o365.nuu.edu.tw>
2025-03-19 17:30:39 +0800

    Create users.js

---
+++ src/app/data/blog/users.js
@@ -0,0 +1,15 @@
+export const users =[
+    {
+        id:1,
+        name:"Emily",
+        email:"emily@gmail.com",
+        pwd:"458529",
+        hashedpwd:"",
+        role:"admin",
+        avatar:""
+    },
+    {
+
+    }
+    
+]
